4f5ada4e580a0770017a0a25b76654e587c92719
mayso2025 <maa57@njit.edu>
2024-12-04 23:22:12 -0500

    Created barebones for a booking form

--- booking-send.php
+++ booking-send.php
@@ -11,30 +11,35 @@ if (isset($argv[1]))
 }
 else
 {
-  $msg = "login request sent";
+  $msg = "booking request sent";
 }
 
-$registration = array(); 
+$booking = array(); 
 
 
 //Sending Registration
-$registration['type'] = "register";
-$registration['username'] = $_POST['username'];
-$registration['password'] = $_POST['password'];
-$registration['email'] = $_POST['email'];
+$booking ['type'] = "booking";
+$booking ['bookerName'] = $_POST['bookerName'];
+$booking ['bookerName'] = $_POST['bookerName'];
+$booking ['numGuest'] = $_POST['numGuest'];
+$booking ['country'] = "Poland"
+$booking ['city'] = "Warsaw";
+$booking ['hotelName'] = "Polish Hotel";
+$booking ['checkinDate'] = $_POST['checkinDate'];
+$booking ['checkOutDate'] = $_POST['checkOutDate'];
 
-$registration['message'] = $msg;
-$response = $client->send_request($registration);
+$booking ['message'] = $msg;
+$response = $client->send_request($booking );
 
 if($response['returnCode'] == 1) //This picks up return code 
 //if the front-end recieves a message from the MQ with a return code of 1, it means the login is successful 
 {
-  echo "YIPPIE"; 
+  echo "Booking successful "; 
   //echo "Heres the username" .$request['username'].   "and heres the password"  .$request['password']; //NOTE: this is just testing to make sure that hte username and password went over
 }
 else if ($response['returnCode'] == 0) //returns user back to login page if login is a failure 
 {
-  echo "womp womp";
+  echo "You did not book a hotel womp womp ";
 }
 
 echo "client received response: ".PHP_EOL;
