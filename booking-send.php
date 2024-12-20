3bded88700b47d5280eff5661ca901d2f0c0e305
mayso2025 <maa57@njit.edu>
2024-12-20 13:46:53 -0500

    made a lot of updates to multiple files

--- booking-send.php
+++ booking-send.php
@@ -21,9 +21,9 @@ $booking = array();
 $booking['type'] = "booking";
 $booking['bookerName'] = $_POST['bookerName'];
 $booking['numGuest'] = $_POST['numGuest'];
-$booking['country'] = "Poland";
-$booking['city'] = "Warsaw";
-$booking['hotelName'] = "Polish Hotel";
+$booking['country'] = $_POST['country'];
+$booking['city'] = $_POST['city']
+$booking['hotelName'] = $_POST['hotelName'];
 $booking['checkinDate'] = $_POST['checkinDate'];
 $booking['checkOutDate'] = $_POST['checkOutDate'];
 
@@ -33,7 +33,8 @@ $response = $client->send_request($booking);
 if($response['returnCode'] == 1) //This picks up return code 
 //if the front-end recieves a message from the MQ with a return code of 1, it means the login is successful 
 {
-  echo "Booking successful "; 
+  header
+  alert("Booking successful"); 
   //echo "Heres the username" .$request['username'].   "and heres the password"  .$request['password']; //NOTE: this is just testing to make sure that hte username and password went over
 }
 else if ($response['returnCode'] == 0) //returns user back to login page if login is a failure 
--- rateandreview.php
+++ rateandreview.php
@@ -11,7 +11,7 @@ if (isset($argv[1]))
 }
 else
 {
-  $msg = "login request sent";
+  $msg = "rating request sent";
 }
 
 $request = array();
@@ -25,25 +25,29 @@ $request['password'] = "password";
 */ 
 //Sending Login
 $review['type'] = "review";
-$review['username'] = $_POST['username'];
+$review['review'] = $_POST['review'];
 $review['password'] = $_POST['password'];
+$review['rate'] = $_POST['rate'] //should be a number 1-5
+$review['photo'] =$_FILES['photo']; //should upload file path up to the db 
 
 
-$request['message'] = $msg;
-$response = $client->send_request($request);
+$review['message'] = $msg;
+$response = $client->send_request($review);
 //$response = $client->publish($request);
 
 
 
 if($response['returnCode'] == 1) //This picks up return code 
-//if the front-end recieves a message from the MQ with a return code of 1, it means the login is successful 
+//if the front-end recieves a message from the MQ with a return code of 1, it means the review send is successful 
 {
-  header("Location: index.php"); 
+  header("Location: booking.php"); 
+  alert("Your review has been sent");
   //echo "Heres the username" .$request['username'].   "and heres the password"  .$request['password']; //NOTE: this is just testing to make sure that hte username and password went over
 }
 else if ($response['returnCode'] == 0) //returns user back to login page if login is a failure 
 {
-  header("Location: index.html");
+  header("Location: booking.php"); 
+  alert("Failed to send review");
 }
 
 echo "client received response: ".PHP_EOL;
